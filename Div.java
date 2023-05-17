43a618cc05e60c533d229b9e7248e69e2df963c3
TusharPawar123 <tusharpawar82018@gmail.com>
2023-05-17 10:49:42 +0530

    my second commit

---
+++ Div.java
@@ -0,0 +1,14 @@
+package Division;
+
+public class Div {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 5;
+int b = 4;
+
+int div = (a-b);
+System.out.println(div);
+	}
+
+}
